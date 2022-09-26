a2c70f6fa3475506e16a97b104218b969c0928ea
bcw4045 <67230834+bcw4045@users.noreply.github.com>
2022-09-27 08:51:38 +0900

    add server.py

--- server.py
+++ server.py
@@ -20,37 +20,43 @@ class SocketServer:
     def socket_access(self, port, ip):
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((self.ip, self.port))
+        server_socket.listen(1)
         return server_socket
 
-    def receive_audio(self):
-        receive_stream = self.p.open(format=self.p.get_format_from_width(width=2), channels=1, rate=self.fs, output=True)
-
-        receive_data = []
-        while True: # 송신받은 음성 재생
-           # if not data:
-            #     break
-            data = self.server_socket.recv(1024)
-            receive_data.append(data)
-
-        receive_stream.stop_stream()
-        receive_stream.close()
-        self.p.terminate()
-
-        print('Finished playback')
-
-    def send_audio(self): # 작업중....
-        # send_stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=self.fs, input=True, frames_per_buffer=self.chunk)
-        for i in range(0, len(self.frames), 1024) :
-            if (i+1024) > len(self.frames):
-                data = self.frames[i:]
-            else:
-                data = self.frames[i, i+1024]
-            self.server_socket.send_all(data)
-
-        print('Finished playback')
+    # def receive_audio(self):
+    #     receive_stream = self.p.open(format=self.p.get_format_from_width(width=2), channels=1, rate=self.fs, output=True)
+    #
+    #     receive_data = []
+    #     while True: # 송신받은 음성 재생
+    #        # if not data:
+    #         #     break
+    #         data = self.server_socket.recv(1024)
+    #         receive_data.append(data)
+    #
+    #     receive_stream.stop_stream()
+    #     receive_stream.close()
+    #     self.p.terminate()
+    #
+    #     print('Finished playback')
+    #
+    # def send_audio(self): # 작업중....
+    #     # send_stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=self.fs, input=True, frames_per_buffer=self.chunk)
+    #     for i in range(0, len(self.frames), 1024) :
+    #         if (i+1024) > len(self.frames):
+    #             data = self.frames[i:]
+    #         else:
+    #             data = self.frames[i, i+1024]
+    #         self.server_socket.send_all(data)
+    #
+    #     print('Finished playback')
 
 
     def echo_test(self):
-        self.client_socket.sandall(b'hello world')
-        data = self.client_socket.recv(1024)
-        print('echo response : ', repr(data))
+        while True:
+            conn, addr = self.server_socket.accept()
+            print('connected by ', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall('echo'+data)
