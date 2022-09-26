bc3b6ff1a43d12a67743c3c8e317c65c6f8f8d34
bcw4045 <67230834+bcw4045@users.noreply.github.com>
2022-09-26 19:38:36 +0900

    socket programming init

---
+++ main.py
@@ -0,0 +1,109 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import socket
+from _thread import *
+import argparse
+import numpy as np
+
+import pyaudio
+
+
+class SocketClient:
+
+    def __init__(self, port, ip):
+        self.port = port
+        self.ip = ip
+        self.client_socket = self.socket_access(self.port, self.ip)
+        self.chunk = 1024
+        self.fs = 16000
+        self.frames = []
+        self.p = pyaudio.PyAudio
+
+    def socket_access(self, port, ip):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((self.ip, self.port))
+        return client_socket
+
+    def record_audio(self):
+        print(f'Recode Starting')
+        stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
+
+        for i in range(0, int(self.fs/self.chunk * 3)):
+            data = stream.read(self.chunk)
+            self.frames.append(data)
+
+        stream.stop_stream()
+        stream.close()
+        self.p.terminate()
+
+        print(f'Recode Finishing')
+
+    def listening_audio(self):
+        if len(self.frames) == 0:
+            print('현재 저장된 녹음이 없습니다....')
+            return 0
+        else:
+            sound = np.array(self.frames)
+
+            sound_bytes = sound.tobytes()
+            stream2 = self.p.open(format=self.p.get_format_from_width(width=2), channels=1, rate=self.fs, output=True)
+            stream2.write(sound_bytes)
+            stream2.stop_stream()
+            stream2.close()
+            self.p.terminate()
+            print(f'저장된 녹음의 재생이 끝났습니다....')
+
+    def receive_audio(self):
+        receive_stream = self.p.open(format=self.p.get_format_from_width(width=2), channels=1, rate=self.fs, output=True)
+
+        receive_data = []
+        while True: # 송신받은 음성 재생
+           # if not data:
+            #     break
+            data = self.client_socket.recv(1024)
+            # receive_data.append(data)
+            receive_stream.write(data)
+
+        receive_stream.stop_stream()
+        receive_stream.close()
+        self.p.terminate()
+
+        print('Finished playback')
+
+    def send_audio(self): # 작업중....
+        # send_stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=self.fs, input=True, frames_per_buffer=self.chunk)
+        self.record_audio()
+        for i in range(0, len(self.frames), 1024) :
+            if (i+1024) > len(self.frames):
+                data = self.frames[i:]
+            else:
+                data = self.frames[i, i+1024]
+            self.client_socket.send_all(data)
+
+
+        print('Finished playback')
+
+
+    def echo_test(self):
+        self.client_socket.sandall(b'hello world')
+        data = self.client_socket.recv(1024)
+        print('echo response : ', repr(data))
+
+# def socket_access(port, ip):
+#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     client_socket.connect((ip, port))
+#     return client_socket
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', required=True, help='연결할 서버의 port번호를 지정해주세요.')
+    parser.add_argument('--ip', required=True, help='연결할 서버의 IP 주소를 지정해주세요.')
+
+    args = parser.parse_args()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+from _thread import *
+import argparse
+import numpy as np
+
+import pyaudio
+
+
+class SocketServer:
+
+    def __init__(self, port, ip):
+        self.port = port
+        self.ip = ip
+        self.server_socket = self.socket_access(self.port, self.ip)
+        self.chunk = 1024
+        self.fs = 16000
+        self.frames = []
+        self.p = pyaudio.PyAudio
+
+    def socket_access(self, port, ip):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.ip, self.port))
+        return server_socket
+
+    def receive_audio(self):
+        receive_stream = self.p.open(format=self.p.get_format_from_width(width=2), channels=1, rate=self.fs, output=True)
+
+        receive_data = []
+        while True: # 송신받은 음성 재생
+           # if not data:
+            #     break
+            data = self.server_socket.recv(1024)
+            receive_data.append(data)
+
+        receive_stream.stop_stream()
+        receive_stream.close()
+        self.p.terminate()
+
+        print('Finished playback')
+
+    def send_audio(self): # 작업중....
+        # send_stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=self.fs, input=True, frames_per_buffer=self.chunk)
+        for i in range(0, len(self.frames), 1024) :
+            if (i+1024) > len(self.frames):
+                data = self.frames[i:]
+            else:
+                data = self.frames[i, i+1024]
+            self.server_socket.send_all(data)
+
+        print('Finished playback')
+
+
+    def echo_test(self):
+        self.client_socket.sandall(b'hello world')
+        data = self.client_socket.recv(1024)
+        print('echo response : ', repr(data))
