4d2212fbf9329a47b76b4183e03e5851fdc00762
bcw4045 <67230834+bcw4045@users.noreply.github.com>
2022-09-27 10:07:57 +0900

    updata main.py

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ import server
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
+    parser.add_argument('user', help='서버와 클라이언트 중에 무엇을 실행시킬지 지정해주세요')
     parser.add_argument('--port', required=True, help='연결할 서버의 port번호를 지정해주세요.')
     parser.add_argument('--ip', required=True, help='연결할 서버의 IP 주소를 지정해주세요.')
-    parser.add_argument('user')
 
     args = parser.parse_args()
+
+    if args.user == 'client':
+        pass
+    else:
+        pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
