ca816b3f2a7ce5d2a388a1e37daa67af906ea864
bcw4045 <67230834+bcw4045@users.noreply.github.com>
2022-10-05 20:40:23 +0900

    update main.py

--- server.py
+++ server.py
@@ -45,6 +45,9 @@ class AudioServer:
         while True:
             data = conn.recv(1024)
             if data in b'end':
+                print(data.rstrip(b'end'))
+                print(list(data.rstrip(b'end')))
+                print(receive_data)
                 receive_data = receive_data + list(data.rstrip(b'end'))
                 break
             receive_data = receive_data + data
