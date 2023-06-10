5ede8d728be54d0c39071150c3df4e8f1baecb91
bcw4045 <67230834+bcw4045@users.noreply.github.com>
2023-06-10 12:35:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--user', required=True, help='서버와 클라이언트 중에 무엇을 실행시킬지 지정해주세요')
-    parser.add_argument('--port', required=True, help='연결할 서버의 port번호를 지정해주세요.', default=28888) # server port : 28888
-    parser.add_argument('--ip', required=True, help='연결할 서버의 IP 주소를 지정해주세요.') # server ip : 172.17.0.4
+    parser.add_argument('--port', required=True, help='연결할 서버의 port번호를 지정해주세요.', default=28888)
+    parser.add_argument('--ip', required=True, help='연결할 서버의 IP 주소를 지정해주세요.')
 
     args = parser.parse_args()
 
